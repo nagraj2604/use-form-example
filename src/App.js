ee4de370286ed0434e6aa7b7f9e7c4084ba04cf2
nagaraj-kambathalli <nagarajkambathalli96@gmail.com>
2023-07-01 18:02:39 +0530

    form validation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup'
 import './App.css';
 
+const customers = [
+  {
+    name: 'Person A',
+    id: 'a'
+  },
+  {
+    name: 'Person B',
+    id: 'b'
+  },
+  {
+    name: 'Person C',
+    id: 'c'
+  },
+]
+const schema = yup.object().shape({
+  selectAll: yup.boolean().when('blessed', {
+    is: (value) => value.length === 0 && value.length !== customers.length,
+    then: (schema) =>
+      schema.oneOf([true], 'select for full')
+  }),
+  blessed: yup.array().when('selectAll', {
+    is: false,
+    then: (schema) => schema.max(1, 'can select one')
+  }),
+}, ['blessed', 'selectAll'])
+
 function App() {
+
+  const { register, setValue, clearErrors, handleSubmit, formState: { errors } } = useForm(({
+    resolver: yupResolver(schema),
+    defaultValues: {
+      blessed: []
+    }
+  }))
+  const selectAllHandler = (value) => {
+    if (value) {
+      setValue('blessed', customers.map((customer) => customer.id));
+      console.log('errors', errors)
+    } else {
+      setValue('blessed', []);
+    }
+    clearErrors(['blessed', 'selectAll'])
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit((data) => { console.log(data) })}>
+        <div>
+          <label> Select All</label>
+          <input type="checkbox" id='selectAll'  {...register('selectAll')} onChange={(e) => selectAllHandler(e.target.checked)} />
+        </div>
+        <p style={{ color: 'red' }}>{errors?.selectAll?.message}</p>
+        <p style={{ color: 'red' }}>{errors?.blessed?.message}</p>
+
+        {customers.map((customer) => (
+          <div key={customer.name}>
+            <div>
+              <p>Name : {customer.name}</p>
+            </div>
+            <div>
+              <label> Blessed</label>
+              <input type="checkbox" id={`blessed-${customer.id}`} {...register('blessed')} value={customer.id} onChange={() => clearErrors('selectAll')} />
+            </div>
+          </div>
+        ))}
+        <input type='submit' />
+      </form>
     </div>
   );
 }
